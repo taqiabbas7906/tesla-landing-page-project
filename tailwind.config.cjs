05763d7d210fec984f651a8aae8d1c62aad0f969
Syed Taqi Abbas <themarriedhorser@gmail.com>
2023-08-14 15:20:31 +0500

    old commit

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,17 @@
 export default {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        accent: {
+          400: "#E51837",
+          300: "#BEBEBE",
+        },
+        primary: {
+          800: "#212121",
+        },
+      },
+    },
   },
   plugins: [],
 };
